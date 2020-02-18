56275b9fe962d2eb209b8d1efba2bbc7293615a7
Djamon Staal <djamon.staal@mybit.nl>
2020-02-18 12:02:57 +0100

    Adds module imports and the config parser.

--- startup.js
+++ startup.js
@@ -2,3 +2,17 @@
 // written to report netbox resources monthly per tenant in pdf.
 // Djamon Staal <djamon.staaal@mybit.nl> - 2020
 
+// Import Modules
+const axios = require('axios')
+const moment = require('moment');
+const nodemailer = require("nodemailer");
+
+// Load config
+try {
+  var config = require('./config');
+} catch (e) {
+  return console.log("Fatal Error: Faild to load the config file.")
+}
+
+// Start the service
+
