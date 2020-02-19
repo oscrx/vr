808287f740b5fbe075d6e35ca03c42105a520412
Oscar Wieman <oscar.wieman@mybit.nl>
2020-02-19 16:20:39 +0100

    make config docker friendly, cleanup

--- startup.js
+++ startup.js
@@ -1,43 +1,40 @@
-// check_mk script to check TCP connections to a remote host.
-// written to report netbox resources monthly per tenant in pdf.
-// Djamon Staal <djamon.staaal@mybit.nl> - 2020
-
 // Import Modules
-const axios = require('axios')
-const moment = require('moment');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config() // use an .env file for configuration in development
+const rp = require('request-promise')
+
+const mailer = require('./handlers/mail')
 
 // Load config
 try {
-  var config = require('./config');
-} catch (e) {
-  return console.log("Fatal Error: Faild to load the config file.")
+  // if (!process.env.NODE_ENV) console.log('Please set the NODE_ENV')
+  if (!process.env.NETBOX_URI) throw new Error('Please enter the netbox uri.')
+  if (!process.env.NETBOX_TOKEN) throw new Error('Please enter the netbox token.')
+  if (!process.env.SMTP_SERVER) throw new Error('Please set an smtp server.')
+  if (!process.env.SMTP_PORT) console.log('No smtp port set.')
+  if (!process.env.SMTP_FROM) throw new Error('Please set a from email address.')
+  if (!process.env.SMTP_TO) throw new Error('Please set some comma seperated email addresses.')
+} catch (error) {
+  console.error(error)
+  console.error('Committing suicide..')
+  process.exit()
 }
 
-var fullVmList;
-
-// Start the service
-getNetboxData('/api/virtualization/virtual-machines/?limit=5', 'get')
-  .then(res => fullVmList = res)
-
-
-
-async function getNetboxData(uri, method) {
-  try {
-    let res = await axios({
-      url: config.netbox_uri + uri,
-      method: 'get',
-      headers: {
-        'accept': 'application/json',
-        'Authorization': 'Token ' + config.netbox_token
-      }
-    })
-    if (res.status == 200) {
-      // test for status you want, etc
-      // console.log(res.status)
-    }
-    // Don't forget to return something
-    return res.data
-  } catch (err) {
-    console.error(err);
+const getNetboxData = async (path, method) => {
+  const options = { // move to envirioment variable?
+    uri: process.env.NETBOX_URI + path,
+    method: method || 'get',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Token ' + process.env.NETBOX_TOKEN
+    },
+    json: true // Automatically parses the JSON string in the response
   }
+  const response = await rp(options)
+  // console.log(response)
+  return response
 }
+
+// use this function asyncronous
+console.log(getNetboxData('/api/virtualization/virtual-machines/?limit=5', 'get'))
+
+mailer()
